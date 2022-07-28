c01e40e59b4fdff65732743996f0bf5c9a86335f
홍성민 <seanhong2000@gmail.com>
2022-07-29 04:49:56 +0900

    chore: finish up settings for pymongo and scrapy

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -6,8 +6,26 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from scrpy.utils.project import get_project_settings
 
+import logging
+
+settings = get_project_settings()
 
 class ScraperPipeline:
     def process_item(self, item, spider):
         return item
+
+    def open_spider(self, spider):
+        conn = MongoClient(settings["MONGODB_SETTINGS"])
+        try:
+            conn.admin.command('ping')
+        except ConnectionFailure:
+            logging.log("Connection failure from MongoClient.")
+
+    def close_spider(self, spider):
+        ...
+
+
